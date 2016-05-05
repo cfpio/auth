545b88681b87ce0e8ae79c0f1357b46df8931023
Michel DAVID <mimah35@gmail.com>
2016-05-05 21:48:02 +0200

    Ajout paramètre de redirection post auth

--- src/main/java/io/cfp/auth/MainCtrl.java
+++ src/main/java/io/cfp/auth/MainCtrl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -19,16 +20,25 @@ public class MainCtrl {
 	private TokenService tokenSrv;
 
 	@RequestMapping("/")
-	public String main(@CookieValue(value = "token", required = false) String token, HttpServletResponse response) {
+	public String main(HttpServletResponse response, @CookieValue(required=false) String token, @CookieValue(required = false) String target, @RequestParam(required=false, value="target") String targetParam) {
 		response.setHeader("Cache-Control","no-cache,no-store,must-revalidate");
 		response.setHeader("Pragma","no-cache");
 		response.setDateHeader("Expires", 0);
 
+		if (targetParam != null) {
+			response.addCookie(new Cookie("target", targetParam));
+			target = targetParam;
+		}
+		
 		if (!tokenSrv.isValid(token)) {
 			return "login";
 		}
+		
+		if (target == null) {
+			target = "http://www.cfp.io";
+		}
 
-		return "home";
+		return "redirect:" + target;
 	}
 	
 	@RequestMapping("/logout")
