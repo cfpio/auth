a93d1687bf8f0236c65807604367a2e008a3f127
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-05-11 09:37:10 +0200

    authenticication via Linkedin

---
+++ src/main/java/io/cfp/auth/api/LinkedInAuthController.java
@@ -0,0 +1,75 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2016, CloudBees, Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ */
+
+package io.cfp.auth.api;
+
+import com.github.scribejava.apis.GoogleApi20;
+import com.github.scribejava.apis.LinkedInApi20;
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value="/linkedin")
+public class LinkedInAuthController extends OauthController {
+
+    @Value("${cfp.linkedin.clientid}")
+    private String clientId;
+    
+    @Value("${cfp.linkedin.clientsecret}")
+    private String clientSecret;
+    
+	@Override
+	protected String getClientId() {
+		return clientId;
+	}
+
+	@Override
+	protected String getClientSecret() {
+		return clientSecret;
+	}
+
+	@Override
+	protected String getScope() {
+		return "r_emailaddress";
+	}
+
+	@Override
+	protected DefaultApi20 getApi() {
+		return LinkedInApi20.instance();
+	}
+
+	@Override
+	protected String getEmailInfoUrl() {
+		return "https://api.linkedin.com/v1/people/~:(email-address)?format=json&oauth2_access_token=";
+	}
+
+	@Override
+	protected String getEmailProperty() {
+		return "emailAddress";
+	}
+}
